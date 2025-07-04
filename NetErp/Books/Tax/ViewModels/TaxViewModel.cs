2afba547bcebc316dda8576418dd85d1dc24fc04
Josue verbel <josueverbel@gmail.com>
2025-07-04 12:31:57 -0500

    iniciando

---
+++ NetErp/Books/Tax/ViewModels/TaxViewModel.cs
@@ -0,0 +1,13 @@
+﻿using Caliburn.Micro;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetErp.Books.Tax.ViewModels
+{
+    public class TaxViewModel : Conductor<object>.Collection.OneActive
+    {
+    }
+}
