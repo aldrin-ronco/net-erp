2fb1b24f1a11706bf3c28d9017238ef934aeb423
Aldrin Alain Ronco Carrillo <aldrin.ronco@gmail.com>
2026-05-05 17:06:09 -0500

    refactor(inventory-config): apply patterns/visual fixes and drop default_aws_s3_config
    - Migrate auto-properties to field keyword
    - Drop unused IEventAggregator dependency
    - Add OnViewReady focus, OnDeactivateAsync close guard
    - Add SeedDefaultValues, ClearSeeds in Undo
    - Visual: redesign with section grouping, 2-col grid, narrower vertical footprint
    - Drop DefaultAwsS3Config (removed from backend schema)

--- NetErp/Inventory/InventoryConfig/ViewModels/InventoryConfigViewModel.cs
+++ NetErp/Inventory/InventoryConfig/ViewModels/InventoryConfigViewModel.cs
@@ -1,5 +1,4 @@
 using Caliburn.Micro;
-using Common.Extensions;
 using Common.Helpers;
 using Common.Interfaces;
 using DevExpress.Mvvm;
@@ -28,42 +27,35 @@ namespace NetErp.Inventory.InventoryConfig.ViewModels
     public class InventoryConfigViewModel : Screen
     {
         private readonly IRepository<InventoryConfigGraphQLModel> _inventoryConfigService;
-        private readonly AwsS3ConfigCache _awsS3ConfigCache;
         private readonly StorageCache _storageCache;
         private readonly NotAnnulledAccountingSourceCache _accountingSourceCache;
-        private readonly IEventAggregator _eventAggregator;
         private readonly JoinableTaskFactory _joinableTaskFactory;
         private readonly INotificationService _notificationService;
         private InventoryConfigGraphQLModel? _loadedConfig;
 
         public InventoryConfigViewModel(
             IRepository<InventoryConfigGraphQLModel> inventoryConfigService,
-            AwsS3ConfigCache awsS3ConfigCache,
             StorageCache storageCache,
             NotAnnulledAccountingSourceCache accountingSourceCache,
-            IEventAggregator eventAggregator,
             JoinableTaskFactory joinableTaskFactory,
             INotificationService notificationService)
         {
             _inventoryConfigService = inventoryConfigService;
-            _awsS3ConfigCache = awsS3ConfigCache;
             _storageCache = storageCache;
             _accountingSourceCache = accountingSourceCache;
-            _eventAggregator = eventAggregator;
             _joinableTaskFactory = joinableTaskFactory;
             _notificationService = notificationService;
         }
 
         #region Properties
 
-        private int _id;
         public int Id
         {
-            get => _id;
+            get;
             set
             {
-                if (_id == value) return;
-                _id = value;
+                if (field == value) return;
+                field = value;
                 NotifyOfPropertyChange(nameof(Id));
                 NotifyOfPropertyChange(nameof(IsNewRecord));
             }
@@ -71,28 +63,26 @@ namespace NetErp.Inventory.InventoryConfig.ViewModels
 
         public bool IsNewRecord => Id == 0;
 
-        private bool _isBusy;
         public bool IsBusy
         {
-            get => _isBusy;
+            get;
             set
             {
-                if (_isBusy == value) return;
-                _isBusy = value;
+                if (field == value) return;
+                field = value;
                 NotifyOfPropertyChange(nameof(IsBusy));
                 NotifyOfPropertyChange(nameof(CanEdit));
                 NotifyOfPropertyChange(nameof(CanSave));
             }
         }
 
-        private bool _isEditing;
         public bool IsEditing
         {
-            get => _isEditing;
+            get;
             set
             {
-                if (_isEditing == value) return;
-                _isEditing = value;
+                if (field == value) return;
+                field = value;
                 NotifyOfPropertyChange(nameof(IsEditing));
                 NotifyOfPropertyChange(nameof(CanEdit));
                 NotifyOfPropertyChange(nameof(CanSave));
@@ -101,119 +91,90 @@ namespace NetErp.Inventory.InventoryConfig.ViewModels
 
         public bool CanEdit => !IsEditing && !IsBusy;
 
-        private AwsS3ConfigGraphQLModel? _selectedDefaultAwsS3Config;
-
-        [ExpandoPath("defaultAwsS3ConfigId", SerializeAsId = true)]
-        public AwsS3ConfigGraphQLModel? SelectedDefaultAwsS3Config
-        {
-            get => _selectedDefaultAwsS3Config;
-            set
-            {
-                if (_selectedDefaultAwsS3Config == value) return;
-                _selectedDefaultAwsS3Config = value;
-                NotifyOfPropertyChange(nameof(SelectedDefaultAwsS3Config));
-                this.TrackChange(nameof(SelectedDefaultAwsS3Config), value);
-                NotifyOfPropertyChange(nameof(CanSave));
-            }
-        }
-
-        private StorageGraphQLModel? _selectedTransitStorage;
-
         [ExpandoPath("transitStorageId", SerializeAsId = true)]
         public StorageGraphQLModel? SelectedTransitStorage
         {
-            get => _selectedTransitStorage;
+            get;
             set
             {
-                if (_selectedTransitStorage == value) return;
-                _selectedTransitStorage = value;
+                if (field == value) return;
+                field = value;
                 NotifyOfPropertyChange(nameof(SelectedTransitStorage));
                 this.TrackChange(nameof(SelectedTransitStorage), value);
                 NotifyOfPropertyChange(nameof(CanSave));
             }
         }
 
-        private AccountingSourceGraphQLModel? _selectedTransferDocAccountingSource;
-
         [ExpandoPath("transferDocAccountingSourceId", SerializeAsId = true)]
         public AccountingSourceGraphQLModel? SelectedTransferDocAccountingSource
         {
-            get => _selectedTransferDocAccountingSource;
+            get;
             set
             {
-                if (_selectedTransferDocAccountingSource == value) return;
-                _selectedTransferDocAccountingSource = value;
+                if (field == value) return;
+                field = value;
                 NotifyOfPropertyChange(nameof(SelectedTransferDocAccountingSource));
                 this.TrackChange(nameof(SelectedTransferDocAccountingSource), value);
                 NotifyOfPropertyChange(nameof(CanSave));
             }
         }
 
-        private AccountingSourceGraphQLModel? _selectedTransferSourceOutAccountingSource;
-
         [ExpandoPath("transferSourceOutAccountingSourceId", SerializeAsId = true)]
         public AccountingSourceGraphQLModel? SelectedTransferSourceOutAccountingSource
         {
-            get => _selectedTransferSourceOutAccountingSource;
+            get;
             set
             {
-                if (_selectedTransferSourceOutAccountingSource == value) return;
-                _selectedTransferSourceOutAccountingSource = value;
+                if (field == value) return;
+                field = value;
                 NotifyOfPropertyChange(nameof(SelectedTransferSourceOutAccountingSource));
                 this.TrackChange(nameof(SelectedTransferSourceOutAccountingSource), value);
                 NotifyOfPropertyChange(nameof(CanSave));
             }
         }
 
-        private AccountingSourceGraphQLModel? _selectedTransferDestinationInAccountingSource;
-
         [ExpandoPath("transferDestinationInAccountingSourceId", SerializeAsId = true)]
         public AccountingSourceGraphQLModel? SelectedTransferDestinationInAccountingSource
         {
-            get => _selectedTransferDestinationInAccountingSource;
+            get;
             set
             {
-                if (_selectedTransferDestinationInAccountingSource == value) return;
-                _selectedTransferDestinationInAccountingSource = value;
+                if (field == value) return;
+                field = value;
                 NotifyOfPropertyChange(nameof(SelectedTransferDestinationInAccountingSource));
                 this.TrackChange(nameof(SelectedTransferDestinationInAccountingSource), value);
                 NotifyOfPropertyChange(nameof(CanSave));
             }
         }
 
-        private AccountingSourceGraphQLModel? _selectedTransferTransitOutAccountingSource;
-
         [ExpandoPath("transferTransitOutAccountingSourceId", SerializeAsId = true)]
         public AccountingSourceGraphQLModel? SelectedTransferTransitOutAccountingSource
         {
-            get => _selectedTransferTransitOutAccountingSource;
+            get;
             set
             {
-                if (_selectedTransferTransitOutAccountingSource == value) return;
-                _selectedTransferTransitOutAccountingSource = value;
+                if (field == value) return;
+                field = value;
                 NotifyOfPropertyChange(nameof(SelectedTransferTransitOutAccountingSource));
                 this.TrackChange(nameof(SelectedTransferTransitOutAccountingSource), value);
                 NotifyOfPropertyChange(nameof(CanSave));
             }
         }
 
-        private AccountingSourceGraphQLModel? _selectedTransferTransitInAccountingSource;
-
         [ExpandoPath("transferTransitInAccountingSourceId", SerializeAsId = true)]
         public AccountingSourceGraphQLModel? SelectedTransferTransitInAccountingSource
         {
-            get => _selectedTransferTransitInAccountingSource;
+            get;
             set
             {
-                if (_selectedTransferTransitInAccountingSource == value) return;
-                _selectedTransferTransitInAccountingSource = value;
+                if (field == value) return;
+                field = value;
                 NotifyOfPropertyChange(nameof(SelectedTransferTransitInAccountingSource));
                 this.TrackChange(nameof(SelectedTransferTransitInAccountingSource), value);
                 NotifyOfPropertyChange(nameof(CanSave));
             }
         }
 
-        public ReadOnlyObservableCollection<AwsS3ConfigGraphQLModel> AwsS3Configs => _awsS3ConfigCache.Items;
         public ReadOnlyObservableCollection<StorageGraphQLModel> Storages => _storageCache.Items;
         public ReadOnlyObservableCollection<AccountingSourceGraphQLModel> AccountingSources => _accountingSourceCache.Items;
 
@@ -264,7 +225,6 @@ namespace NetErp.Inventory.InventoryConfig.ViewModels
                 IsBusy = true;
 
                 await Task.WhenAll(
-                    _awsS3ConfigCache.EnsureLoadedAsync(),
                     _storageCache.EnsureLoadedAsync(),
                     _accountingSourceCache.EnsureLoadedAsync());
 
@@ -287,6 +247,21 @@ namespace NetErp.Inventory.InventoryConfig.ViewModels
             await base.OnInitializedAsync(cancellationToken);
         }
 
+        protected override void OnViewReady(object view)
+        {
+            base.OnViewReady(view);
+            this.SetFocus(() => SelectedTransitStorage);
+        }
+
+        protected override Task OnDeactivateAsync(bool close, CancellationToken cancellationToken)
+        {
+            if (close)
+            {
+                this.AcceptChanges();
+            }
+            return base.OnDeactivateAsync(close, cancellationToken);
+        }
+
         #endregion
 
         #region Load
@@ -294,41 +269,37 @@ namespace NetErp.Inventory.InventoryConfig.ViewModels
         private async Task LoadInventoryConfigAsync()
         {
             var (_, query) = _loadQuery.Value;
-            var config = await _inventoryConfigService.GetSingleItemAsync(query, new { });
+            InventoryConfigGraphQLModel? config = await _inventoryConfigService.GetSingleItemAsync(query, new { });
 
             if (config is not null)
             {
                 _loadedConfig = config;
                 Id = config.Id;
 
-                _selectedDefaultAwsS3Config = AwsS3Configs.FirstOrDefault(x => x.Id == config.DefaultAwsS3Config?.Id);
-                NotifyOfPropertyChange(nameof(SelectedDefaultAwsS3Config));
-
-                _selectedTransitStorage = Storages.FirstOrDefault(x => x.Id == config.TransitStorage?.Id);
-                NotifyOfPropertyChange(nameof(SelectedTransitStorage));
-
-                _selectedTransferDocAccountingSource = AccountingSources.FirstOrDefault(x => x.Id == config.TransferDocAccountingSource?.Id);
-                NotifyOfPropertyChange(nameof(SelectedTransferDocAccountingSource));
-
-                _selectedTransferSourceOutAccountingSource = AccountingSources.FirstOrDefault(x => x.Id == config.TransferSourceOutAccountingSource?.Id);
-                NotifyOfPropertyChange(nameof(SelectedTransferSourceOutAccountingSource));
-
-                _selectedTransferDestinationInAccountingSource = AccountingSources.FirstOrDefault(x => x.Id == config.TransferDestinationInAccountingSource?.Id);
-                NotifyOfPropertyChange(nameof(SelectedTransferDestinationInAccountingSource));
-
-                _selectedTransferTransitOutAccountingSource = AccountingSources.FirstOrDefault(x => x.Id == config.TransferTransitOutAccountingSource?.Id);
-                NotifyOfPropertyChange(nameof(SelectedTransferTransitOutAccountingSource));
-
-                _selectedTransferTransitInAccountingSource = AccountingSources.FirstOrDefault(x => x.Id == config.TransferTransitInAccountingSource?.Id);
-                NotifyOfPropertyChange(nameof(SelectedTransferTransitInAccountingSource));
+                SelectedTransitStorage = Storages.FirstOrDefault(x => x.Id == config.TransitStorage?.Id);
+                SelectedTransferDocAccountingSource = AccountingSources.FirstOrDefault(x => x.Id == config.TransferDocAccountingSource?.Id);
+                SelectedTransferSourceOutAccountingSource = AccountingSources.FirstOrDefault(x => x.Id == config.TransferSourceOutAccountingSource?.Id);
+                SelectedTransferDestinationInAccountingSource = AccountingSources.FirstOrDefault(x => x.Id == config.TransferDestinationInAccountingSource?.Id);
+                SelectedTransferTransitOutAccountingSource = AccountingSources.FirstOrDefault(x => x.Id == config.TransferTransitOutAccountingSource?.Id);
+                SelectedTransferTransitInAccountingSource = AccountingSources.FirstOrDefault(x => x.Id == config.TransferTransitInAccountingSource?.Id);
 
                 SeedCurrentValues();
             }
+            else
+            {
+                SeedDefaultValues();
+            }
+        }
+
+        private void SeedDefaultValues()
+        {
+            this.ClearSeeds();
+            this.AcceptChanges();
+            NotifyOfPropertyChange(nameof(CanSave));
         }
 
         private void SeedCurrentValues()
         {
-            this.SeedValue(nameof(SelectedDefaultAwsS3Config), SelectedDefaultAwsS3Config);
             this.SeedValue(nameof(SelectedTransitStorage), SelectedTransitStorage);
             this.SeedValue(nameof(SelectedTransferDocAccountingSource), SelectedTransferDocAccountingSource);
             this.SeedValue(nameof(SelectedTransferSourceOutAccountingSource), SelectedTransferSourceOutAccountingSource);
@@ -345,26 +316,14 @@ namespace NetErp.Inventory.InventoryConfig.ViewModels
 
         private void Undo()
         {
-            _selectedDefaultAwsS3Config = AwsS3Configs.FirstOrDefault(x => x.Id == _loadedConfig?.DefaultAwsS3Config?.Id);
-            NotifyOfPropertyChange(nameof(SelectedDefaultAwsS3Config));
+            this.ClearSeeds();
 
-            _selectedTransitStorage = Storages.FirstOrDefault(x => x.Id == _loadedConfig?.TransitStorage?.Id);
-            NotifyOfPropertyChange(nameof(SelectedTransitStorage));
-
-            _selectedTransferDocAccountingSource = AccountingSources.FirstOrDefault(x => x.Id == _loadedConfig?.TransferDocAccountingSource?.Id);
-            NotifyOfPropertyChange(nameof(SelectedTransferDocAccountingSource));
-
-            _selectedTransferSourceOutAccountingSource = AccountingSources.FirstOrDefault(x => x.Id == _loadedConfig?.TransferSourceOutAccountingSource?.Id);
-            NotifyOfPropertyChange(nameof(SelectedTransferSourceOutAccountingSource));
-
-            _selectedTransferDestinationInAccountingSource = AccountingSources.FirstOrDefault(x => x.Id == _loadedConfig?.TransferDestinationInAccountingSource?.Id);
-            NotifyOfPropertyChange(nameof(SelectedTransferDestinationInAccountingSource));
-
-            _selectedTransferTransitOutAccountingSource = AccountingSources.FirstOrDefault(x => x.Id == _loadedConfig?.TransferTransitOutAccountingSource?.Id);
-            NotifyOfPropertyChange(nameof(SelectedTransferTransitOutAccountingSource));
-
-            _selectedTransferTransitInAccountingSource = AccountingSources.FirstOrDefault(x => x.Id == _loadedConfig?.TransferTransitInAccountingSource?.Id);
-            NotifyOfPropertyChange(nameof(SelectedTransferTransitInAccountingSource));
+            SelectedTransitStorage = Storages.FirstOrDefault(x => x.Id == _loadedConfig?.TransitStorage?.Id);
+            SelectedTransferDocAccountingSource = AccountingSources.FirstOrDefault(x => x.Id == _loadedConfig?.TransferDocAccountingSource?.Id);
+            SelectedTransferSourceOutAccountingSource = AccountingSources.FirstOrDefault(x => x.Id == _loadedConfig?.TransferSourceOutAccountingSource?.Id);
+            SelectedTransferDestinationInAccountingSource = AccountingSources.FirstOrDefault(x => x.Id == _loadedConfig?.TransferDestinationInAccountingSource?.Id);
+            SelectedTransferTransitOutAccountingSource = AccountingSources.FirstOrDefault(x => x.Id == _loadedConfig?.TransferTransitOutAccountingSource?.Id);
+            SelectedTransferTransitInAccountingSource = AccountingSources.FirstOrDefault(x => x.Id == _loadedConfig?.TransferTransitInAccountingSource?.Id);
 
             SeedCurrentValues();
             IsEditing = false;
@@ -440,7 +399,6 @@ namespace NetErp.Inventory.InventoryConfig.ViewModels
             var fields = FieldSpec<InventoryConfigGraphQLModel>
                 .Create()
                 .Field(f => f.Id)
-                .Select(f => f.DefaultAwsS3Config, nested: a => a.Field(a => a.Id))
                 .Select(f => f.TransitStorage, nested: s => s.Field(s => s.Id))
                 .Select(f => f.TransferDocAccountingSource, nested: a => a.Field(a => a.Id))
                 .Select(f => f.TransferSourceOutAccountingSource, nested: a => a.Field(a => a.Id))
@@ -461,7 +419,6 @@ namespace NetErp.Inventory.InventoryConfig.ViewModels
                 .Create()
                 .Select(selector: f => f.Entity, alias: "entity", overrideName: "inventoryConfig", nested: sq => sq
                     .Field(f => f.Id)
-                    .Select(f => f.DefaultAwsS3Config, nested: a => a.Field(a => a.Id))
                     .Select(f => f.TransitStorage, nested: s => s.Field(s => s.Id))
                     .Select(f => f.TransferDocAccountingSource, nested: a => a.Field(a => a.Id))
                     .Select(f => f.TransferSourceOutAccountingSource, nested: a => a.Field(a => a.Id))
@@ -487,7 +444,6 @@ namespace NetErp.Inventory.InventoryConfig.ViewModels
                 .Create()
                 .Select(selector: f => f.Entity, alias: "entity", overrideName: "inventoryConfig", nested: sq => sq
                     .Field(f => f.Id)
-                    .Select(f => f.DefaultAwsS3Config, nested: a => a.Field(a => a.Id))
                     .Select(f => f.TransitStorage, nested: s => s.Field(s => s.Id))
                     .Select(f => f.TransferDocAccountingSource, nested: a => a.Field(a => a.Id))
                     .Select(f => f.TransferSourceOutAccountingSource, nested: a => a.Field(a => a.Id))
